d9ea27ea8290701cec3893786d6ad0aa45f01672
Francis Painchaud <44142080+Darkxys@users.noreply.github.com>
2024-06-26 21:50:45 -0400

    updated cash display

--- src/components/text/Cash.tsx
+++ src/components/text/Cash.tsx
@@ -6,7 +6,8 @@ export default function CashTextDisplay({ amount }: Props) {
   return (
     <div className="w-fit">
       <p className={amount < 0 ? 'text-red-400' : 'text-green-400'}>
-        {amount < 0 ? '-' : '+'}{Math.abs(amount).toFixed(2)} $
+        {amount < 0 ? '-' : '+'}
+        {Math.abs(amount).toLocaleString('en-US', { minimumFractionDigits: 2, maximumFractionDigits: 2 })} $
       </p>
     </div>
   )
